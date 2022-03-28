75a3f82e72d7e3cfa01b104fb24f85941c782359
Rachel de Araujo Rosa Ferreira <rachel.ferreira@nava.com.br>
2022-03-28 16:04:51 -0300

    Componentes Angular

---
+++ src/app/primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro',
+  template: `<div>
+    <p>Sou um Componente criado de forma manual</p>
+  </div>`
+})
+export class PrimeiroComponent {}
